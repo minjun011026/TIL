86f8a2994edbf910d5a6145359395ed591831878
minjun011026 <jun011026@gmail.com>
2025-03-18 23:03:03 +0900

    docs: 3장 완

--- 코틀린 쿡북/test.kt
+++ 코틀린 쿡북/test.kt
@@ -1,9 +1,21 @@
-data class tmp(
-    val d : Int,
-    var s : Int
+data class Tmp(
+    val name : String,
+    val ss : Int
 )
 
 fun main() {
-    val a = tmp(1,2)
-    val b = a.component2()
+    for (n in 1..10) {
+        val x = when (n % 3) {
+            0 -> "$n % 3 == 0"
+            1 -> "$n % 3 == 1"
+            2 -> "$n % 3 == 2"
+            else -> throw Exception("나올 수가 없는디")
+        }
+    }
+    val tmp = throw Exception("1")
+}
+fun ss(): Nothing = throw Exception("1")
+
+fun sss() {
+    throw Exception("@")
 }
