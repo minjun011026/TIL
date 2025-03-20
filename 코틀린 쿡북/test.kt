c325839566a87277ac64bb7122710d5e00b83f8c
minjun011026 <jun011026@gmail.com>
2025-03-20 19:14:41 +0900

    docs: 코틀린 쿡북 4장완

--- 코틀린 쿡북/test.kt
+++ 코틀린 쿡북/test.kt
@@ -1,6 +1,20 @@
-fun sum(vararg num : Int) = num.reduce{ acc,n -> acc+ 3 * n}
-
+import java.math.BigInteger
 
 fun main() {
-    sum(1,2,3)
+
 }
+
+tailrec fun fact(n: Long, acc: BigInteger = BigInteger.ONE) : BigInteger =
+    when(n) {
+        0L -> acc
+        1L -> acc
+        else -> fact(n-1, acc * BigInteger.valueOf(n))
+    }
+
+
+tailrec fun fact(n : Long) : BigInteger =
+    when(n) {
+        0L -> BigInteger.ONE
+        1L -> BigInteger.ONE
+        else -> BigInteger.valueOf(n) * fact(n-1)
+    }
