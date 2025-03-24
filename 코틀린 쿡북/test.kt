5d4e3adc3aba4e3ec3f9db6142727121463dae5b
minjun011026 <jun011026@gmail.com>
2025-03-24 23:52:48 +0900

    docs: 코틀린 쿡북 5장완

--- 코틀린 쿡북/test.kt
+++ 코틀린 쿡북/test.kt
@@ -1,20 +1,45 @@
-import java.math.BigInteger
+import java.time.LocalDate
+
+class LocalDateProgression(
+    override val start: LocalDate,
+    override val endInclusive: LocalDate,
+    val step: Long = 1
+) : Iterable<LocalDate>, ClosedRange<LocalDate> {
+    override fun iterator(): Iterator<LocalDate> =
+        LocalDateProgressionIterator(start, endInclusive, step)
+
+    infix fun step(days: Long) = LocalDateProgression(start, endInclusive, days)
+}
+
+internal class LocalDateProgressionIterator(
+    first: LocalDate,
+    val last: LocalDate,
+    val step: Long
+) : Iterator<LocalDate> {
+
+    private var current = first
+
+    override fun hasNext(): Boolean = current <= last
+
+    override fun next(): LocalDate {
+        val next = current
+        current = current.plusDays(step)
+        return next
+    }
+}
+
+
+operator fun LocalDate.rangeTo(other: LocalDate) =
+    LocalDateProgression(this, other)
 
 fun main() {
+    val startDate = LocalDate.now()
+    val midDate = startDate.plusDays(3)
+    val endDate = startDate.plusDays(5)
+
+    val dateRange = startDate..endDate
+
+    for (day in dateRange) println(day)
 
 }
 
-tailrec fun fact(n: Long, acc: BigInteger = BigInteger.ONE) : BigInteger =
-    when(n) {
-        0L -> acc
-        1L -> acc
-        else -> fact(n-1, acc * BigInteger.valueOf(n))
-    }
-
-
-tailrec fun fact(n : Long) : BigInteger =
-    when(n) {
-        0L -> BigInteger.ONE
-        1L -> BigInteger.ONE
-        else -> BigInteger.valueOf(n) * fact(n-1)
-    }
