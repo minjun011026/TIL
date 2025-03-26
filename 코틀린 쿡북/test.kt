f4f5638b9073c596163e37d2458f63589c65ad78
minjun011026 <jun011026@gmail.com>
2025-03-26 22:21:14 +0900

    docs: 코틀린 쿡북 6장완

--- 코틀린 쿡북/test.kt
+++ 코틀린 쿡북/test.kt
@@ -3,13 +3,6 @@ import kotlin.math.ceil
 import kotlin.math.sqrt
 
 fun main() {
-    fun numberSequence() = sequence {
-        var num = 1
-        while(true){
-            yield(num)
-            num += 1
-        }
-    }
-    val nums = numberSequence().take(10).toList()
-    println(nums)
+
 }
+fun ss(string: String) = string.let { "HHIHIHI" }
