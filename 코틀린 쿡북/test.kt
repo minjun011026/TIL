710c1a60dd20beac8c92ef4d1309879a502e1348
minjun011026 <jun011026@gmail.com>
2025-03-26 20:54:50 +0900

    docs: 코틀린 쿡북 6장완

--- 코틀린 쿡북/test.kt
+++ 코틀린 쿡북/test.kt
@@ -1,45 +1,15 @@
-import java.time.LocalDate
-
-class LocalDateProgression(
-    override val start: LocalDate,
-    override val endInclusive: LocalDate,
-    val step: Long = 1
-) : Iterable<LocalDate>, ClosedRange<LocalDate> {
-    override fun iterator(): Iterator<LocalDate> =
-        LocalDateProgressionIterator(start, endInclusive, step)
-
-    infix fun step(days: Long) = LocalDateProgression(start, endInclusive, days)
-}
-
-internal class LocalDateProgressionIterator(
-    first: LocalDate,
-    val last: LocalDate,
-    val step: Long
-) : Iterator<LocalDate> {
-
-    private var current = first
-
-    override fun hasNext(): Boolean = current <= last
-
-    override fun next(): LocalDate {
-        val next = current
-        current = current.plusDays(step)
-        return next
-    }
-}
-
-
-operator fun LocalDate.rangeTo(other: LocalDate) =
-    LocalDateProgression(this, other)
+import java.util.*
+import kotlin.math.ceil
+import kotlin.math.sqrt
 
 fun main() {
-    val startDate = LocalDate.now()
-    val midDate = startDate.plusDays(3)
-    val endDate = startDate.plusDays(5)
-
-    val dateRange = startDate..endDate
-
-    for (day in dateRange) println(day)
-
+    fun numberSequence() = sequence {
+        var num = 1
+        while(true){
+            yield(num)
+            num += 1
+        }
+    }
+    val nums = numberSequence().take(10).toList()
+    println(nums)
 }
-
